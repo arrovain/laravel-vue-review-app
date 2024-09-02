895e600f26c0b633173d01fd95a41fff11380bf5
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 10:34:53 +0300

    template renders a ProductList component, passing the products data as a prop and ceate a reactive data object with an empty products array and lastly fetch it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,11 @@ import Toast from 'vue-toastification'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 createApp(App)
 
 .use(Toast)
+.use(router)
 
 mount('#app')
