2f0eee57d3c924f01d1b25aa3f62e757a2e89fc7
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 11:16:13 +0300

    sends a put request to update the review, resets the form data, emits a 'reviewUpdated' event with the response data, and shows a success toast message

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
   
         public function run(): void
         {
-            
+            User::factory(10)->create();
           $this->call([
               ProductSeeder::class,
           
