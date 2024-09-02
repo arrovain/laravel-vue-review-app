7739b485955e5682a9a9888f55fa9e21fc835c20
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 11:30:51 +0300

    indicates that a Product has many Reviews. It's setting up a one-to-many relationship

--- frontend/src/components/reviews/AddReview.vue
+++ frontend/src/components/reviews/AddReview.vue
@@ -1,74 +1,99 @@
 <template>
-    <div class="card mb-2">
-      <div class="card-header bg-white">
-          <h5 class="text-center mt-2">
-              Reviews ({{ props.reviews.length }})
-          </h5>
-      </div>
-      <div class="card-body">
-          <ul class="mt-4 list-group">
-              <li class="list-group-item d-flex justify-content-between align-items-start"
-                  v-for="review in props.reviews"
-                  :key="review.id"
-                  >
-                  <div class="ms-2 me-auto">
-                      <div class="fw-bold">
-                          {{ review.title }}
-                      </div>
-                      <p>
-                          {{ review.body }}
-                      </p>
-                      <p class="cart-text">
-                          <small class="text-body-seconda">
-                              by {{ review.user.name }} - <span class="text-danger">{{ review.created_at }}</span>
-                          </small>
-                      </p>
-                      <p>
-                          <StarRating 
-                              v-model:rating="review.rating"
-                              :show-rating="false"
-                              read-only
-                              :star-size="24"
-                          />
-                      </p>
-                  </div>
-                  <div class="d-flex flex-column align-items-center">
-                      <button class="btn btn-sm btn-danger mb-2"
-                          @click="removeReview(review)">
-                          <i class="bi bi-trash"></i>
-                      </button>
-                      <button class="btn btn-sm btn-warning mb-2"
-                          @click="editReview(review)">
-                          <i class="bi bi-pencil"></i>
-                      </button>
-                  </div>
-              </li>
-          </ul>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-      import StarRating from 'vue-star-rating'
-  
-      const props = defineProps({
-          reviews: {
-              type: Array,
-              required: true
-          }
-      })
-  
-      const emit = defineEmits(['editReviewEvent', 'removeReviewEvent'])
-  
-      const editReview = (review) => {
-          emit('editReviewEvent', review)
-      }
-  
-      const removeReview = (review) => {
-          emit('removeReviewEvent', review.id)
-      }
-  </script>
-  
-  <style>
-  
-  </style>
+    <form class="mt-5">
+        <div class="mb-3">
+            <label for="title">Title*</label>
+            <input type="text" name="title" id="title"
+                v-model="data.review.title"
+                class="form-control"
+                placeholder="Title">
+        </div>
+        <div class="mb-3">
+            <label for="body">Body*</label>
+            <textarea 
+                rows="3"
+                cols="30"
+                name="body" id="body"
+                v-model="data.review.body"
+                class="form-control"
+                placeholder="Body"></textarea>
+        </div>
+        <div class="mb-3">
+            <StarRating 
+                v-model:rating="data.review.rating"
+                :show-rating="false"
+            />
+        </div>
+        <div class="mb-3">
+            <button class="btn btn-dark"
+                :disabled="disabled"
+                @click.prevent="storeReview">
+                Submit
+            </button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+    import { computed, reactive } from 'vue'
+    import StarRating from 'vue-star-rating'
+    import axios from "axios"
+    import { useToast } from 'vue-toastification'
+
+    const toast = useToast()
+
+    const data = reactive({
+        review: {
+            title: '',
+            body: '',
+            rating: 0
+        }
+    })
+
+    const props = defineProps({
+        product: {
+            type: Object,
+            required: true
+        }
+    })
+
+    const emit = defineEmits(['reviewAdded'])
+
+    const disabled = computed(() => {
+        if(!data.review.title || !data.review.body || data.review.rating === 0){
+            return true
+        }else {
+            false
+        }
+    })
+
+    const storeReview = async () => {
+        try {
+            const response = await axios.post(`http://127.0.0.1:8000/api/review/${props.product.id}/store`,
+                {
+                    title: data.review.title,
+                    body: data.review.body,
+                    rating: data.review.rating,
+                    user_id: Math.floor(Math.random() * 10) + 1 
+                }
+            )
+            data.review = {
+                title: '',
+                body: '',
+                rating: 0
+            }
+
+            emit('reviewAdded', response.data.data)
+
+            toast.success('Review has been added successfully', {
+                timeout: 2000
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
+
+<style>
+
+</style>
