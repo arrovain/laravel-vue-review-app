892f86678856da25e5a993fbcf2b94ffce89559b
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 10:11:42 +0300

    main.js settings for vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,15 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'vue-toastification/dist/index.css'
+import Toast from 'vue-toastification'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+
+.use(Toast)
+
+mount('#app')
