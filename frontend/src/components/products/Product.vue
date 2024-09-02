9ddb8466170de12f5c33ac9d64750908ba0f805d
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 10:48:52 +0300

    display data product, both AddReview and UpdateReview components receive the product as a prop and emit events to update the product data

---
+++ frontend/src/components/products/Product.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="row my-5" v-if="data.product">
+      <div class="col-md-10 mx-auto">
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img :src="data.product.image" alt="Product Image" 
+                  class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-text">
+                  {{  data.product.name }}
+                </h5>
+                <p class="card-text">
+                  {{ data.product.desc }}
+                </p>
+                <p>
+                  <small class="text-body-secondary">
+                    $ {{  data.product.price }}
+                  </small>
+                </p>
+                <p v-if="data.product.reviews.length > 0">
+                  <StarRating 
+                    v-model:rating="reviewAvg"
+                    read-only
+                    :star-size="24"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="data.product.reviews.length > 0">
+          <ReviewsList 
+            :reviews="data.product.reviews"
+            @editReviewEvent="editReview"
+            @removeReviewEvent="deleteReview"
+          />
+        </div>
+        <div class="my-3">
+          <div class="card">
+            <div class="card-header bg-white text-center">
+              <h5 class="mt-2" v-if="!data.reviewToUpdate.updating">Add a review</h5>
+              <h5 class="mt-2" v-else>Edit a review</h5>
+            </div>
+            <div class="card-body">
+              <AddReview 
+                v-if="!data.reviewToUpdate.updating"
+                :product="data.product"
+                @reviewAdded="setProduct"
+              />
+              <UpdateReview 
+                v-else
+                :reviewToUpdate="data.reviewToUpdate.data"
+                :product="data.product"
+                @cancelUpdating="cancelUpdating"
+                @reviewUpdated="setProduct"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
