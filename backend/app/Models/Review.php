97f1dd10da5fb514f9014b3e1cada9dfa7f84b6e
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 09:51:39 +0300

    laravel accessor

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Review extends Model
 {
@@ -11,6 +13,11 @@ class Review extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
-        
+
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
     }
 }
