620ca4b0675fc06ef5fb753356b0bde67565f7a7
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 10:00:14 +0300

    routes for review and product. the {product} is a route parameter, likely representing a product id.

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ReviewController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('product/{product}/show', [ProductController::class, 'show']);
+Route::post('review/{product}/store', [ReviewController::class, 'store']);
+Route::put('review/{product}/{review}/update', [ReviewController::class, 'update']);
+Route::post('review/{product}/delete', [ReviewController::class, 'delete']);
